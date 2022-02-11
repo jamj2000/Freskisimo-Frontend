56e62b05b801e37a150676f36cebded494623bf7
Juan Antonio González <jgonzalezrivera@eci.safanet.es>
2022-02-11 12:13:19 +0100

    Minimun update

--- src/App.svelte
+++ src/App.svelte
@@ -7,8 +7,9 @@
 <header>
 	<Nav/>
 </header>
+<main>
 	<Main/>
-	
+</main>
 <Footer/>
 
 <style>
@@ -17,13 +18,6 @@
 		padding: 1em;
 		max-width: 240px;
 		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
 	}
 
 	@media (min-width: 640px) {
