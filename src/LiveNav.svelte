2594e16e1352a99983e0f05505eb0089764e4410
Juan Antonio González <jgonzalezrivera@eci.safanet.es>
2022-02-21 19:36:54 +0100

    changed livenav

--- src/LiveNav.svelte
+++ src/LiveNav.svelte
@@ -1,8 +1,18 @@
 <script>
-    import { Link } from "svelte-routing";    
+  import { Link } from "svelte-routing";
 </script>
 
-<nav> 
-    <Link to="/">Inicio</Link>
-    <Link to="/congelados">Congelados</Link>		
-</nav>
+<ul class="nav nav-tabs justify-content-center" role="tablist">
+  <li class="nav-item" role="tab">
+    <Link to="/" class="nav-link" onclick:active>Inicio</Link>
+  </li>
+  <li class="nav-item" role="tab">
+    <Link to="/congelados" class="nav-link" onclick:active>Congelados</Link>
+  </li>
+  <li class="nav-item" role="tab">
+    <Link to="/vehiculos" class="nav-link" onclick:active>Vehículos</Link>
+  </li>
+  <li class="nav-item" role="tab">
+    <Link to="/repartidores" class="nav-link" onclick:active>Repartidores</Link>
+  </li>
+</ul>
