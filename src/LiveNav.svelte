27481d3f99480cf1c9627e75a3c22e09514cb4b2
Juan Antonio González <jgonzalezrivera@eci.safanet.es>
2022-02-22 16:34:04 +0100

    active tab fixed

--- src/LiveNav.svelte
+++ src/LiveNav.svelte
@@ -1,18 +1,30 @@
 <script>
   import { Link } from "svelte-routing";
+
+  export let to = "";
+
+  function getProps({ location, href, isPartiallyCurrent, isCurrent }) {
+    const isActive = href === "/" ? isCurrent : isPartiallyCurrent || isCurrent;
+
+    // The object returned here is spread on the anchor element's attributes
+    if (isActive) {
+      return { class: "nav-link active" };
+    }
+    return { class: "nav-link" };
+  }
 </script>
 
 <ul class="nav nav-tabs justify-content-center" role="tablist">
   <li class="nav-item" role="tab">
-    <Link to="/" class="nav-link" onclick:active>Inicio</Link>
+    <Link to="/" {getProps}>Inicio</Link>
   </li>
   <li class="nav-item" role="tab">
-    <Link to="/congelados" class="nav-link" onclick:active>Congelados</Link>
+    <Link to="/congelados" {getProps}>Congelados</Link>
   </li>
   <li class="nav-item" role="tab">
-    <Link to="/vehiculos" class="nav-link" onclick:active>Vehículos</Link>
+    <Link to="/vehiculos" {getProps}>Vehículos</Link>
   </li>
   <li class="nav-item" role="tab">
-    <Link to="/repartidores" class="nav-link" onclick:active>Repartidores</Link>
+    <Link to="/repartidores" {getProps}>Repartidores</Link>
   </li>
 </ul>
